1fe4b5e5cf10a3a3382b70b86781a2d4136e7303
Fernando Garcia <ferisagaragu@gmail.com>
2020-10-16 02:04:23 -0500

    Test para authentication en servicio implementado corregido

--- src/test/kotlin/org/pechblenda/exershiprest/service/AuthServiceImplTest.kt
+++ src/test/kotlin/org/pechblenda/exershiprest/service/AuthServiceImplTest.kt
@@ -1,19 +1,20 @@
 package org.pechblenda.exershiprest.service
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.runner.RunWith
+
 import org.pechblenda.exershiprest.dao.IUserDAO
 import org.pechblenda.exershiprest.entity.User
 
 import org.pechblenda.exershiprest.service.`interface`.IAuthService
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
@@ -21,13 +22,13 @@ import org.springframework.transaction.annotation.Transactional
 
 import java.util.UUID
 
-@SpringBootTest
 @Transactional
-@AutoConfigureTestEntityManager
+@SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner::class)
-@TestPropertySource(locations = [ "classpath:application-test.properties" ])
+@TestPropertySource(locations = ["classpath:application-test.properties"])
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@WithMockUser(username = "admin", authorities = ["ADMIN", "USER"])
+@WithMockUser(username = "fakeUserImpl", authorities = ["ADMIN", "USER"])
 class AuthServiceImplTest {
 
 	@Autowired
@@ -36,32 +37,45 @@ class AuthServiceImplTest {
 	@Autowired
 	private lateinit var userDAO: IUserDAO
 
+	@Autowired
+	private lateinit var encoder: PasswordEncoder
+
 	private var userMount: User? = null
 
 	@BeforeAll
 	fun beforeAll() {
-		userMount = userDAO.save(
-			User(
-				uid = UUID.randomUUID(),
-				name = "admin",
-				lastName = "",
-				userName = "admin",
-				email = "no-real@fake.com",
-				password = "fake",
-				enabled = true,
-				active = true,
-				activatePassword = UUID.randomUUID(),
-				photo = "",
-				refreshToken = null,
-				roles = mutableListOf()
-			)
-		)
+		restoreUser()
 	}
 
-	//sign-up
+	fun restoreUser() {
+		if (userMount != null) {
+			userMount = userDAO.findById(userMount!!.uid).orElse(null)
+			userMount!!.enabled = false
+			userMount!!.active = false
+			userMount!!.activatePassword = UUID.randomUUID()
+			userMount = userDAO.save(userMount!!)
+		} else {
+			userMount = userDAO.save(
+				User(
+					uid = UUID.randomUUID(),
+					name = "fakeUserNameImpl",
+					lastName = "",
+					userName = "fakeUserImpl",
+					email = "no-realImpl@fake.com",
+					password = encoder.encode("fake"),
+					enabled = true,
+					active = true,
+					activatePassword = UUID.randomUUID(),
+					photo = "",
+					refreshToken = null,
+					roles = mutableListOf()
+				)
+			)
+		}
+	}
+
 	@Test
 	fun `test validate if token is valid`() {
-		println(authService.validateToken().body)
 		assertEquals(
 			((authService.validateToken().body as Map<String, Any>)["data"]
 				as Map<String, Any>)["validateToken"],
